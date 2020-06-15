e416c922d98db357642726e45ea26db6b8943697
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-06-15 21:00:59 +0900

    Hash  Level2 새로고침

--- Programmers(Dart)/src/com/programmers/level2/hash/PhoneBook_Hash.java
+++ Programmers(Dart)/src/com/programmers/level2/hash/PhoneBook_Hash.java
@@ -1,34 +1,24 @@
 package com.programmers.level2.hash;
 import java.util.*;
-/**
- * Hash를 활용한 코드 But 여러번 for문을 쓴걸로봐서 효율적이진 않음
- * @author user
- *
- */
-public class PhoneBook_Hash {
-	    public static boolean solution(String[] phone_book) {
-	        boolean answer = true;
-	        HashMap<String, Integer> map = new HashMap<String, Integer>();
-	        for(int inx = 0; inx < phone_book.length; inx++) {
-	            for(int jnx = 1; jnx < phone_book[inx].length(); jnx++) {
-	                map.put(phone_book[inx].substring(0, jnx), 1);
-	                System.out.println("inx: "+inx+" jnx: "+jnx+" phone_book: "+phone_book[inx].substring(0, jnx));
-	            }
-	            System.out.println();
-	        }
-	        for(int inx = 0; inx < phone_book.length; inx++) {
-	            if(map.containsKey(phone_book[inx])) {
-	                answer = false;
-	                break;
-	            } else {
-	                map.put(phone_book[inx], 1);
-	            }
-	        }
-	        return answer;
-	    }
-	    
-	    public static void main(String[]args) {
-	    	String[] phone_book= {"119", "97674223", "1195524421"};
-	    	solution(phone_book);
-	    }
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Map<String,String> map = new HashMap<>();//map은 순서가없는데 LinkedHashMap은 순서있음
+        
+        for(String phone:phone_book){
+            map.put(phone,"prefix");
+        }
+        System.out.println(map);
+        
+        for(String phone:phone_book){
+            for(int i=0; i<phone.length();i++){
+                if(map.containsKey(phone.substring(0,i))) return false;
+            }
+        }
+        
+        
+        return answer;
+    }
 }
