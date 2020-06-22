bacbf64e28e1bf5a5e31169099c1a503185cd570
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-06-22 19:50:20 +0900

    Prime Commit

--- Programmers(Dart)/src/com/programmers/level2/priorityQue/DoublePriority.java
+++ Programmers(Dart)/src/com/programmers/level2/priorityQue/DoublePriority.java
@@ -1,5 +1,34 @@
 package com.programmers.level2.priorityQue;
+import java.util.*;
 
 public class DoublePriority {
+	    public int[] solution(String[] operations) {
+	        int[] answer = new int[2];
+	        PriorityQueue<Integer> aPq=new PriorityQueue<>();
+	        PriorityQueue<Integer> dPq=new PriorityQueue<>(Collections.reverseOrder());
+	        for(int i=0; i<operations.length; i++){
+	            if(operations[i].contains("I")){
+	                String [] tmp=operations[i].split(" ");
+	                dPq.offer(Integer.parseInt(tmp[1]));
+	                aPq.offer(Integer.parseInt(tmp[1]));
+	            }else if(!dPq.isEmpty() && operations[i].equals("D 1")){
+	                int max=dPq.peek();
+	                dPq.remove(max);
+	                aPq.remove(max);
+	            }else if(!dPq.isEmpty() && operations[i].equals("D -1")){
+	                int min=aPq.peek();
+	                dPq.remove(min);
+	                aPq.remove(min);
+	            }
+	        }
+	        if(dPq.isEmpty() ||aPq.isEmpty()){
+	            answer[0]=0;
+	            answer[1]=0;
+	        }else{
+	            answer[0]=dPq.poll();
+	            answer[1]=aPq.poll();
+	        }        
+	        return answer;
+	    }
 
 }
--- Programmers(Dart)/src/com/programmers/level2/sort/FindPrimeNum.java
+++ Programmers(Dart)/src/com/programmers/level2/sort/FindPrimeNum.java
@@ -2,10 +2,24 @@ package com.programmers.level2.sort;
 
 public class FindPrimeNum {
 
+	public static boolean prime(int n) {
+		if(n%2==0) {
+			return false;
+		}else if(n%3==0) {
+			return false;
+		}else if(n%5==0) {
+			return false;
+		}else if(n%7==0) {
+			return false;
+		}
+		return true;
+	}
 	public static int solution(String numbers) {
 		int answer=0; 
 		String[]tmp=numbers.split("");
-		
+		for(int i=0; i<tmp.length; i++) {
+			
+		}
 		
 		
 		return answer;
