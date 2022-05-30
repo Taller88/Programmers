810a82b02f634b6beb8cac3fe9d74da4af1c963b
JinwooJung <iod1124@coocon.net>
2022-05-31 08:26:18 +0900

    MenuRenuel 자바

--- Programmers(Dart)/src/com/programmers/level2/sense/MenuRenuel.java
+++ Programmers(Dart)/src/com/programmers/level2/sense/MenuRenuel.java
@@ -1,46 +1,75 @@
 package com.programmers.level2.sense;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class MenuRenuel {
-	public static void main(String[]args) {
-		MenuRenuel mr = new MenuRenuel();
+
+	public static void main(String[] args) {
 		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-		int[] course = {2,3,5};
-		
+		int[] course = {2,3,4};
+		MenuRenuel mr = new MenuRenuel();
 		String[] result = mr.solution(orders, course);
+		for(int i =0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
 		
+//"AC", "ACDE", "BCFG", "CDE"
 	}
-	static HashMap<String ,Integer> map = new HashMap<>();
+	static HashMap<String, Integer> map = new HashMap<String, Integer>();
+	static int intMaxSize = Integer.MAX_VALUE;
+	static int intMinSize = Integer.MIN_VALUE;
 	
 	public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        HashMap<String, Integer> map = new HashMap<>();
-		for(int i=0; i<course.length; i++) {
+		String[] answer = {};
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0 ; i<course.length; i++) {
 			int combiLen = course[i];
-			
+			map = new HashMap<String, Integer>();
+			intMinSize = Integer.MIN_VALUE;
+//			System.out.println(combiLen);
 			for(int j=0; j<orders.length; j++) {
-				char[] arr = orders[i].toCharArray();
+				char[] arr = orders[j].toCharArray();
 				Arrays.sort(arr);
-				combination(arr,"", 0, combiLen);
+//				System.out.println(combiLen+": "+orders[i]);
+				combination(arr, combiLen, 0, "",0);
+			}
+			for(String key: map.keySet()) {
+				int obj = map.get(key);
+				if(obj>intMinSize) {
+					intMinSize = obj;
+				}
+			}
+			for(String key: map.keySet()) {
+				int obj = map.get(key);
+				if(intMinSize>=2 && obj == intMinSize) {
+					result.add(key);
+				}
 			}
 		}
-		System.out.println(map.size());
-        
-        return answer;
-    }
-	private void combination(char[] orders,String result ,int idx, int combiLen) {
-		if(result.length()== combiLen) {
-			System.out.println(combiLen +": "+result);
 
+		answer = new String[result.size()];
+		for(int i =0; i<result.size(); i++) {
+			answer[i] = result.get(i);
+		}
+		Arrays.sort(answer);
+		return answer;
+	}
+
+
+	private void combination(char[] arr, int combiLen, int cnt, String result, int currentIdx) {
+		if(combiLen  == cnt) {
 			map.put(result, map.getOrDefault(result, 0)+1);
 			return;
 		}
-		for(int i=idx; i<orders.length; i++) {
-			combination(orders, result+orders[i], idx+1, combiLen);
+		
+		for(int i = currentIdx; i<arr.length; i++) {
+			combination(arr, combiLen, cnt+1, result+arr[i], i+1);
 		}
-		return;
+
+		
 	}
 
 }
+
